980490e606f4833e5bc03f7c3abbee755138eab2
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2025-05-13 09:19:53 +0530

    ToDo List

---
+++ src/app/(projects)/todolist/_components/Cards.tsx
@@ -0,0 +1,9 @@
+const Cards = ({
+  item,
+}: Readonly<{
+  item: string;
+}>) => {
+  return <div>Cards: {item}</div>;
+};
+
+export default Cards;
--- src/app/(projects)/todolist/page.tsx
+++ src/app/(projects)/todolist/page.tsx
@@ -1,5 +1,39 @@
+'use client';
+import { useState } from 'react';
+import Cards from './_components/Cards';
+import Button from '@/components/Button';
+
 const ToDoList = () => {
-  return <>To Do List</>;
+  const [inputTxt, setInputTxt] = useState<string>('');
+  const [toDoList, setToDoList] = useState<string[]>([]);
+
+  const addItem = () => {
+    setToDoList((prev) => [...prev, inputTxt]);
+    setInputTxt('');
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen bg-black text-white">
+      <div className="flex">To Do List</div>
+      <div className="flex flex-col">
+        <input
+          type="text"
+          name="inputTxt"
+          id="inputTxt"
+          value={inputTxt}
+          onChange={(e) => setInputTxt(e.target.value)}
+          className="border-2 border-amber-50 px-4 py-2 rounded-full"
+          placeholder="Type Something..."
+        />
+        <Button buttonText="Add Item" onClick={addItem} type="submit" className="mt-4" />
+      </div>
+      <div className="flex">
+        {toDoList.map((item, index) => (
+          <Cards key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ToDoList;
