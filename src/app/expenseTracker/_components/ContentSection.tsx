281191ec44dc8bcc5f4945112ba42341d2626d44
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2025-06-11 02:43:49 +0530

    Left Section

--- src/app/expenseTracker/_components/ContentSection.tsx
+++ src/app/expenseTracker/_components/ContentSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ContentSection = () => {
-  return <div className="bg-white text-black px-4 py-8 rounded-2xl">ContentSection</div>;
+  return <div className="bg-white text-black px-4 py-8 rounded-2xl w-2/3">ContentSection</div>;
 };
 
 export default ContentSection;
--- src/app/expenseTracker/_components/LeftMenu.tsx
+++ src/app/expenseTracker/_components/LeftMenu.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { User } from 'lucide-react';
 
 const LeftMenu = () => {
-  return <div className="w-1/4">LeftMenu</div>;
+  return (
+    <div className="w-1/4">
+      <div className="flex flex-col">
+        <span>
+          <User />
+        </span>
+        <span className="font-bold">Pushpenbder</span>
+        <span>pushpendersingh311@gmail.com</span>
+      </div>
+      <div className="flex flex-col"></div>
+    </div>
+  );
 };
 
 export default LeftMenu;
